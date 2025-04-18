6f28dd9ed8b393647bce4b23e7efac3278ccdc51
Emre Arslan <emrearslanea8@gmail.com>
2025-04-18 18:48:52 +0300

    update

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,7 +3,6 @@ import { Locale, getMessages } from '@/i18n';
 import { getFontFamily } from '@/theme/theme';
 import { NextIntlClientProvider } from 'next-intl';
 import { getTranslations } from 'next-intl/server';
-import Script from 'next/script';
 import { DrawerProvider } from '@/contexts/DrawerContext';
 import ThemeRegistry from '@/theme/ThemeRegistry';
 import Header from '@/components/Header';
@@ -19,21 +18,9 @@ export default async function RootLayout({
     <html lang={locale}>
       <head>
         {isProduction && (
-          <Script
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-NQCZV9J');`,
-            }}
-          />
-        )}
-        {isProduction && (
           <meta
             name="google-site-verification"
-            content="HcwWovrpIyigp0wjGgeXgoC8r7dPlUkXJZ2JRqEcA6Y"
+            content="dvkqN8gH79SW_bgzgQQuwFNwxNMxCsEy_iZxl-h8FOY"
           />
         )}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
